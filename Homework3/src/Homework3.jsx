3c264bcce9239b7022e8b0a0fbe44a4223b280c3
vlad <Jnrhjqcz123>
2021-10-30 14:40:29 +0300

    Homework 3 is done

--- Homework3/src/Homework3.jsx
+++ Homework3/src/Homework3.jsx
@@ -1,71 +1,30 @@
 import React from "react";
-import CustomInput from "./components/CustomInput";
 import List from "./components/List";
+
 
 class Homework3 extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ``,
-            tmpTxt: `on`,
-            tasks: []
+            products: []
         }
-        this.changeText = this.changeText.bind(this);
-        this.addTask = this.addTask.bind(this);
-        this.press = this.press.bind(this);
     }
-
-
-    addTask(text) {
-        let arr = this.state.tasks;
-        arr.push(text);
-        this.setState({
-            tasks: arr
-        })
-    }
-    changeText(e, key) {
-
-        this.setState({
-            [key]: e.target.value
-        })
-    }
-
     componentDidMount() {
         console.log('Component did mounted');
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+                console.log(json);
+                this.setState({
+                    products: json
+                })
+            });
     }
-
-    static getDerivedStateFromProps(state, props) {
-        console.log('getDerivedStateFromProps');
-        return null
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log('componentDidUpdate-->>  ', prevState.tmpTxt, this.state.tmpTxt );
-        return prevState !== this.state.tmpTxt;
-       /* return true*/
-    }
-    press() {
-        this.setState({
-            tmpTxt: this.state.tmpTxt === 'on' ? 'off' : 'on'
-        })
-    }
-
     render() {
-        console.log('render');
-        const {text} = this.state;
-        const {tmpTxt} = this.state;
+        const {products} = this.state;
         return (
             <>
-                <CustomInput type={"text"} placeholder={"add your task"} value={text} change={(e,key)=>this.changeText(e,'text')}/>
-                <button onClick={()=> {
-                    text && this.addTask(text);
-                    this.setState({
-                        text: ''
-                    })
-                }}>Add Task</button>
-                <button onClick={this.press}>{tmpTxt}</button>
-                <List tasks={this.state.tasks}/>
-
+                <List tasks={products}/>
             </>
         )
     }
--- Homework3/src/components/List/index.jsx
+++ Homework3/src/components/List/index.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import './style.css'
-import Task from "../Task";
 
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sum: 0
+        }
+        this.updateCart = this.updateCart.bind(this);
+    }
+updateCart(price) {
+    this.setState({
+        sum: Math.round((this.state.sum + Number((price)))*100)/100
+    })
+}
 render() {
    const {tasks} = this.props
     return (
         <>
-        <h4>List of tasks</h4>
-        <ol className={'list'}>
-            {tasks.map(e=>{
-                return <Task text={e}/>
-            })}
-        </ol>
+        <ul className={'list'}>
+            {tasks.length? tasks.map((e, key)=>{
+                return <>
+                    <li className='task' key={e.id}>
+                        <p>{e.title}</p>
+                        <img className={'pic'} src={e.image} alt=""/>
+                        <a href="#" onClick={()=> {
+                            this.updateCart(e.price);
+                        }}>Add</a>
+                    </li>
+            </>
+            }) : <h4>Loading...</h4>}
+        </ul>
+            <div className={'cart'}>
+                <h1>{this.state.sum}$</h1>
+            </div>
             </>
     )
 }
